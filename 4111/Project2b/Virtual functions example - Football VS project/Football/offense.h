bed581a6dac1e933d66e67e32140755f97f624a7
Joy <abedinjoynal2@gmail.com>
2018-01-11 09:32:32 -0500

    mini proj part2

---
+++ 4111/Project2b/Virtual functions example - Football VS project/Football/offense.h
@@ -0,0 +1,29 @@
+/*
+ * This is the header file for the player class for CSC 4111 Lab 1 Task 2
+ * You are not allowed to change this file. 
+ * 10 points will be subtracted if this file is modified.
+ *
+ * @Author C. Dorman
+ */
+
+// Question 3 - What do the #ifndef, #define and #endif preprocessor commands do in this file? 
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+#include <string>
+#include "offense.h"
+using namespace std;
+
+class Player :public Offense {
+
+	int yards;
+
+	Offense(string name);
+	setMinutesPlayed(int minutes);
+	setYards(int yards);
+	printStats() const;
+};
+
+
+#endif
---
+++ 4111/Project2b/Virtual functions example - Football VS project/Virtual functions example - Football VS project/Football/defense.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "defense.h"
+
+using namespace std;
+
+
+	defense::defense(string name) {
+}
+	setMinutesPlayed(int minutes);
+	setTackles(int tackles);
+	printStats() const;
+};
