2834236c158c444bcef7bf2ae1fdc09c70bfdb58
Joy <abedinjoynal2@gmail.com>
2018-01-31 10:01:49 -0500

    lab01 4421 completed

--- opSystem/4421/Joynal_abedin_Lab01_4421/lab02Part1/shownames.c
+++ opSystem/4421/Joynal_abedin_Lab01_4421/lab02Part1/shownames.c
@@ -1,25 +1,30 @@
 #include <dirent.h>
-#include <errno.h> #include <stdio.h>
+#include <errno.h> 
+#include <stdio.h>
+
 int main (int argc, char *argv[])
 {
 struct dirent *direntp; 
 // define a pointer with type directory stream (DIR):
-________
-
+DIR *dir;
 // check the number of arguments (argc). It should be two. If it
 // is not two, then print an error message and exit the program:
-if (__________)
+if (argc != 2)
 { 
     fprintf (stderr, "Usage: %s directory name \n", argv[0]); 
     return 1;
 }
+
+
+
+
 
 // The following lines should open the directory given by
 //   argument argv[1] (by using opendir). 
 // Store it in your defined directory stream variable.
 // The rest checks if the system call returned a proper value
 //   if not an error message is printed and the program closed
-if ( ________ == NULL) 
+if ( (dir = opendir (argv[1]))  == NULL) 
 { 
             perror ( "Failed to open directory" ); return 1;
 }
@@ -28,11 +33,12 @@ if ( ________ == NULL)
 // direntp. readdir will read one entry at a time and increment
 // automatically. This is why it is in a while loop. 
 // Then, print all the file names (using the struct from readdir).
-while (_______ != NULL)  
-           printf ("%s \n" , _______ );
+while ((direntp = readdir (dir)) != NULL)  
+           printf ("%s \n" , direntp->d_name );
+           
 
 // close the defined directory stream.
-while ( ( closedir (_______) == -1) && ( errno == EINTR) ); 
+while ( ( closedir (dir) == -1) && ( errno == EINTR) ); 
 
 return 0;
 }
--- opSystem/4421/Joynal_abedin_Lab01_4421/lab02Part2/printaccess.c
+++ opSystem/4421/Joynal_abedin_Lab01_4421/lab02Part2/printaccess.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-#include <time.h> #include <sys/stat.h>
+#include <time.h> 
+#include <sys/stat.h>
 int main (int argc, char *argv[])
 {
 
     // define an instance of struct stat (don’t use a pointer).
-    ________
-
+    //________
+    struct stat buffer;
     // Use the stat function store the status of the file in your stat struct
     // stat takes a pointer, so pass your struct by reference with &
-    if ( _____ == -1)
+    //stat (const *char, struct stat * )
+    if ( stat(argv[1], &buffer) == -1)
     { 
         // Use perror to print error message then exit program
-        ______
+        //______
+        perror("Error");
+        return 1;
     }
     // print the last access time for the file using defined instance of
     // struct stat. ctime requires a pointer, hence the & below.
-    printf ("%s last accessed at %s", argv[1], ctime(&_____.st_atime));
+    printf ("%s last accessed at %s", argv[1], ctime(&buffer.st_atime));
 }
