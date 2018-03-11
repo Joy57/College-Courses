961c955974e0fcea1128d6c85f1876ad59f149eb
Joy <abedinjoynal2@gmail.com>
2018-03-10 21:10:40 -0500

    lab07 update2

--- operatingSystem/LAB 4421/lab07/Asg_pipe_o.c
+++ operatingSystem/LAB 4421/lab07/Asg_pipe_o.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+
 #define BUFSIZE 10
 int main(void) {
     char bufin[BUFSIZE] = "empty";
@@ -9,22 +10,24 @@ int main(void) {
     int bytesin;
     pid_t childpid;
     int fd[2];
-    if (pipe()==-1) { /* create a pipe */
+    int ret;
+    ret = pipe(fd);
+    if (ret == -1) { /* create a pipe */
         perror("Failed to create the pipe");
         return 1;
     }
     bytesin = strlen(bufin);
     childpid = fork();
-    if (fork()== -1) {
+    if (childpid == -1) {
         perror("Failed to fork");
         return 1;
     }
-    if (ppid() == 0)
+    if (childpid == 0)
     /* child code */
-        write(bytesin, bufout, strlen(bufout)+1);
+      write(fd[1], bufout, strlen(bufout)+1);
     else
     /* parent code */
-        bytesin = read(bytesin, bufin, BUFSIZE);
+        bytesin = read(fd[0], bufin, BUFSIZE);
     fprintf(stderr, "[%ld]:my bufin is {%.*s}, my bufout is {%s}\n", (long)getpid(), bytesin, bufin, bufout);
     return 0;
 }
