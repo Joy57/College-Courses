9d7ceb6ed91ffd88050cb930e44d86cfe07fcc4f
Joy <abedinjoynal2@gmail.com>
2018-03-18 14:47:20 -0400

    finished lab

--- operatingSystem/LAB 4421/lab07/Asg_red_o.c
+++ operatingSystem/LAB 4421/lab07/Asg_red_o.c
@@ -11,27 +11,28 @@ int main(void) {
         perror("Failed to setup pipeline");
         return 1;
     }
-    int pid = fork();
-    if (pid == -1) { /* fork a child */
+    childpid = fork();
+    if (childpid < 0) { /* fork a child */
         perror("Failed to fork a child");
         return 1;
     }
-    if (pid == 0) {/* env is the child */
-        
+    if (childpid == 0) {/* env is the child */
+        close(fd[0]);
         if (dup2(fd[1],STDOUT_FILENO) == -1)
             perror("Failed to redirect stdout of env");
-        else if (close(fd[0]) == -1) /* close unused file descriptor */
+        else if (close(fd[1]) == -1) /* close unused file descriptor */
             perror("Failed to close extra pipe descriptors on env");
         else {
-            execl("usr/bin/env", "env", NULL); /* execute env */
+            execl("/usr/bin/env", "env", NULL); /* execute env */
             perror("Failed to exec env");
         }
         return 1;
     }
-    if (dup2(fd[1], STDOUT_FILENO) == -1)
+    close(fd[1]);
+    if (dup2(fd[0], STDIN_FILENO) == -1)
     /* grep is the parent */
         perror("Failed to redirect stdin of grep");
-    else if (close(fd[1]) == -1)
+    else if (close(fd[0]) == -1)
         perror("Failed to close extra pipe file descriptors on grep");
     else {
         execl("/bin/grep", "grep", "HOME", NULL); /*execute "grep HOME"*/
